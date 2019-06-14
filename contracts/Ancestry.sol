f367002f70cb01e57de8b625e85543351db1f135
Remco Bloemen <remco@0x.org>
2019-06-13 17:59:03 -0700

    Add Ancestry

---
+++ contracts/Ancestry.sol
@@ -0,0 +1,97 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "contracts/CreateAddress.sol";
+import "contracts/Cloning.sol";
+
+contract Ancestry is CreateAddress, Cloning {
+
+    // The root of the family tree.
+    address  public PROGENITOR;
+    uint256[] public BIRTH_CERTIFICATE;
+    uint256 public next_nonce;
+
+    modifier only_relatives(uint256[] memory proof) {
+        require(descends(PROGENITOR, msg.sender, proof), "NOT_RELATED");
+        _;
+    }
+
+    // TODO: only_ancestors, only_parents, only_children, only_offspring, only_sibblings
+
+    constructor(
+        address progenitor,
+        uint256[] memory birthCerificate
+    )
+        public
+    {
+        initialize(progenitor, birthCerificate);
+    }
+
+    function initialize(
+        address progenitor,
+        uint256[] memory birthCerificate
+    )
+        public
+    {
+        require(PROGENITOR == address(0x0), "ALREADY_INITIALIZED");
+        if (progenitor == address(0)) {
+            progenitor = address(this);
+        }
+        require(descends(progenitor, address(this), birthCerificate), "INVALID_BIRTH_CERTIFICATE");
+        PROGENITOR = progenitor;
+        BIRTH_CERTIFICATE = birthCerificate;
+
+        // Constract child nonces start at 1
+        next_nonce = 1;
+    }
+
+    /// Check if a given contract is a sibbling
+    /// @param progenitor The root of the family tree.
+    /// @param descendant The constract address to check for membership in the family tree.
+    /// @param birthCertificate Claimed proof of ancenstry.
+    function descends(
+        address progenitor,
+        address  descendant,
+        uint256[] memory birthCertificate
+    )
+        public pure
+        returns (bool)
+    {
+        address relative = progenitor;
+        for (uint256 i = 0; i < birthCertificate.length; ++i) {
+            relative = create1(relative, birthCertificate[i]);
+        }
+        return relative == descendant;
+    }
+
+    /// Issues a birth certificate for the next child contract.
+    /// NOTE: The birth certificate must be used in the next CREATE.
+    /// NOTE: A birth certificate must be requested for each CREATE. 
+    function issueBirthCertificate()
+        public
+        returns (uint256[] memory birthCertificate)
+    {
+        birthCertificate = new uint256[](BIRTH_CERTIFICATE.length + 1);
+
+        // Copy over exsisting certificate
+        for(uint256 i = 0; i < BIRTH_CERTIFICATE.length; ++i) {
+            birthCertificate[i] = BIRTH_CERTIFICATE[i];
+        }
+
+        // Append new entry
+        birthCertificate[BIRTH_CERTIFICATE.length] = next_nonce;
+
+        // Increment CREATE nonce
+        next_nonce += 1;
+    }
+
+    function procreate()
+        public
+        returns (Ancestry child)
+    {
+        child = Ancestry(spawn());
+        child.initialize(
+            PROGENITOR,
+            issueBirthCertificate()
+        );
+    }
+}
---
+++ test/Ancestry.js
@@ -0,0 +1,27 @@
+const Ancestry = artifacts.require("Ancestry");
+
+contract("Ancestry", accounts => {
+    it("should procreate.", async () => {
+        const instance = await Ancestry.new("0x0000000000000000000000000000000000000000", []);
+        console.log(instance.address);
+        const child1tx = await instance.procreate();
+        const child1 = await Ancestry.at(child1tx.logs[0].args.child);
+        const child2tx = await instance.procreate();
+        const child2 = await Ancestry.at(child2tx.logs[0].args.child);
+        const child21tx = await child2.procreate();
+        const child21 = await Ancestry.at(child21tx.logs[0].args.child);
+
+        assert.equal(await instance.PROGENITOR(), instance.address);
+        assert.equal((await instance.next_nonce()).toNumber(), 3);
+        assert.equal(await child1.PROGENITOR(), instance.address);
+        assert.equal((await child1.BIRTH_CERTIFICATE(0)).toNumber(), 1);
+        assert.equal((await child1.next_nonce()).toNumber(), 1);
+        assert.equal(await child2.PROGENITOR(), instance.address);
+        assert.equal((await child2.BIRTH_CERTIFICATE(0)).toNumber(), 2);
+        assert.equal((await child2.next_nonce()).toNumber(), 2);
+        assert.equal(await child21.PROGENITOR(), instance.address);
+        assert.equal((await child21.BIRTH_CERTIFICATE(0)).toNumber(), 2);
+        assert.equal((await child21.BIRTH_CERTIFICATE(1)).toNumber(), 1);
+        assert.equal((await child21.next_nonce()).toNumber(), 1);
+    });
+});
